f4a11baacd4411c49a8450da62b92fc1dc5a6e17
Reid Lynch <reid.lynch@gmail.com>
2023-10-08 20:04:05 -0400

    reserved words

--- jlox/impl/Scanner.java
+++ jlox/impl/Scanner.java
@@ -1,7 +1,9 @@
 package jlox.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static jlox.impl.TokenType.*;
 
@@ -14,6 +16,28 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -99,6 +123,8 @@ class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
@@ -170,6 +196,16 @@ class Scanner {
     return c >= '0' && c <= '9';
   }
 
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
   private void number() {
     while (isDigit(peek()))
       advance();
@@ -183,8 +219,18 @@ class Scanner {
         advance();
     }
 
-    addToken(NUMBER,
-        Double.parseDouble(source.substring(start, current)));
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek()))
+      advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null)
+      type = IDENTIFIER;
+    addToken(type);
   }
 
 }
